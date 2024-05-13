07904a3481c112c160e4262c58c09788e9cb1185
Yusuf AL <yusufal5558@gmail.com>
2024-05-13 16:38:58 +0300

    controllers

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
 
 const app = express()
+
+//Connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/smartedu-db')
+.then(() => {
+    console.log("DB Connected Successfully");
+}).catch(() => {
+    console.log("DB Connection Error");
+});
 
 //Template Engine
 app.set("view engine", "ejs")
 
 //Middlewares
 app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name: "index"
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
-    })
-})
+app.use('/', pageRoute)
+app.use('/courses', courseRoute)
 
 const port = 3000;
 app.listen(port, () => {
